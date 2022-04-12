3282c6e117fa6a8dbe5c9a53b4cd04c84a6dabc9
saint-lag <maiasena.gabriel@gmail.com>
2022-04-11 23:32:58 -0300

    Updates

---
+++ precache-manifest.5498aff080ae29f3de193497bd3cb0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a57b1d84d3b55d9ac93be06ed4e774",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "5750bc98683055f0bcf4",
+    "url": "/trivia-game/static/css/main.c631786e.chunk.css"
+  },
+  {
+    "revision": "ca2d89234d04f4518083",
+    "url": "/trivia-game/static/js/2.4f6e1aa3.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia-game/static/js/2.4f6e1aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5750bc98683055f0bcf4",
+    "url": "/trivia-game/static/js/main.6a8e44ca.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
